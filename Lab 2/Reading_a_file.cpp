ec2b6ec214abae2de30d5efcaffc45219ec13c8a
QAISER ALI <qaisers.official@gmail.com>
2026-02-28 22:45:04 +0500

    Add file reading example with error handling and output display

---
+++ Lab 2/Reading_a_file.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream file("FileName.txt");
+    string line;
+
+    if (!file) {
+        cout << "File not found!" << endl;
+        return 1;
+    }
+    cout << "Contents of the file:";
+
+    while (getline(file, line)) {
+        
+        cout << line << endl;
+    }
+
+    file.close();
+    return 0;
+}
