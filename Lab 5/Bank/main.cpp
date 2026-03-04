b6e0a5a666f9be3ac7009b0a685dfbd1724ffc3e
QAISER ALI <qaisers.official@gmail.com>
2026-03-04 14:26:45 +0500

    Update comments in main.cpp to reflect Bank Account Management task and objectives

--- Lab 5/Bank/main.cpp
+++ Lab 5/Bank/main.cpp
@@ -15,6 +15,12 @@ int main() {
     return 0;
 }
 /*
-Lab Task 1: Create a class Circle that has private members for radius. 
-Implement methods to set the radius, calculate the area, 
-and display the radius and area. Split the program into Circle.h, Circle.cpp, and main.cpp.*/
+Lab Task 3: Bank Account Management
+Objective:
+A.	Implement a simple bank account class with basic functionalities.
+B.	Understand how to structure real-world applications with proper file organisation.
+Theory:
+A bank account class can have private members for the account balance and
+ account number. Functions can be implemented to deposit, withdraw, and check the balance.
+
+ */
